3903e963870553bae1c7f5b82ba114506ccba773
Bammidi Pragati Rao <bpragatirao@gmail.com>
2026-02-02 23:20:09 +0530

    Time: 0 ms (100%), Space: 19.5 MB (12.58%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,5 +3,16 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        nums.sort()
+        l,m,r = 0,0,len(nums)-1
+        while m<=r:
+            if nums[m]==0:
+                nums[l],nums[m]=nums[m],nums[l]
+                l+=1
+                m+=1
+            elif nums[m]==1:
+                m+=1
+            else:
+                nums[m],nums[r]=nums[r],nums[m]
+                r-=1
+
         
