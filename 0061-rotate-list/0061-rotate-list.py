012077ccb6c448de10aeca7c70cfce5e11670a31
Bammidi Pragati Rao <bpragatirao@gmail.com>
2026-02-18 15:58:02 +0530

    Time: 0 ms (100%), Space: 19.5 MB (29.87%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.py
+++ 0061-rotate-list/0061-rotate-list.py
@@ -9,22 +9,22 @@ class Solution:
         if not head or not head.next or k == 0:
             return head
 
-        length = 1
-        tail = head
-        while tail.next:
-            tail = tail.next
-            length += 1
+        n = 1
+        node = head
+        while node.next:
+            node = node.next
+            n += 1
 
-        tail.next = head
+        node.next = head
 
-        k = k % length
-        steps_to_new_tail = length - k
+        k = k % n
+        steps = n - k
         
-        new_tail = head
-        for i in range(steps_to_new_tail - 1):
-            new_tail = new_tail.next
+        new_node = head
+        for i in range(steps - 1):
+            new_node = new_node.next
             
-        new_head = new_tail.next
-        new_tail.next = None
+        new_head = new_node.next
+        new_node.next = None
         
         return new_head
