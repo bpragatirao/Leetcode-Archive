65b36c081eb1bf9218817b7265e43d4fe2aa94af
Bammidi Pragati Rao <bpragatirao@gmail.com>
2026-03-01 21:56:15 +0530

    Time: 23 ms (43.79%), Space: 19.4 MB (86.79%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -2,17 +2,17 @@ class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         candidates.sort()
         res = []
-        def sum2(start, current_subset, current_sum):
-            if current_sum == target:
-                res.append(list(current_subset))
+        def sum2(start, curr, curr_sum):
+            if curr_sum == target:
+                res.append(list(curr))
                 return
-            if current_sum > target:
+            if curr_sum > target:
                 return
             for i in range(start, len(candidates)):
                 if i > start and candidates[i] == candidates[i - 1]:
                     continue
-                current_subset.append(candidates[i])
-                sum2(i + 1, current_subset, current_sum + candidates[i])
-                current_subset.pop()
+                curr.append(candidates[i])
+                sum2(i + 1, curr, curr_sum + candidates[i])
+                curr.pop()
         sum2(0, [], 0)
         return res
