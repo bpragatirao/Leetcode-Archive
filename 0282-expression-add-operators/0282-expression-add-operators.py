ec17f393ee025c68e7e0ceb166236ff54bf098c6
Bammidi Pragati Rao <bpragatirao@gmail.com>
2026-03-11 20:12:59 +0530

    Time: 383 ms (64.56%), Space: 19.5 MB (97.05%) - LeetHub

---
+++ 0282-expression-add-operators/0282-expression-add-operators.py
@@ -0,0 +1,30 @@
+class Solution:
+    def addOperators(self, num: str, target: int) -> List[str]:
+        res = []
+        
+        def solve(i, path, curr, prev_val):
+            if i == len(num):
+                if curr == target:
+                    res.append(path)
+                return
+
+            for j in range(i, len(num)):
+                if j > i and num[i] == '0':
+                    break
+                
+                curr_str = num[i : j + 1]
+                curr_val = int(curr_str)
+
+                if i == 0:
+                    solve(j + 1, curr_str, curr_val, curr_val)
+                else:
+                    solve(j + 1, path + "+" + curr_str, curr + curr_val, curr_val)
+                    
+                    solve(j + 1, path + "-" + curr_str, curr - curr_val, -curr_val)
+
+                    solve(j + 1, path + "*" + curr_str, 
+                              (curr - prev_val) + (prev_val * curr_val), 
+                              prev_val * curr_val)
+
+        solve(0, "", 0, 0)
+        return res
